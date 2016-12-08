a58f6102b5d0a7456eb75d8762b1a302ac5c9284
Nik Shkrob <n.shkrob+gh@gmail.com>
2016-12-08 06:16:44 -0800

    drag!

--- graphs.js
+++ graphs.js
@@ -13,40 +13,77 @@ stage.interactive = true;
 
 
 function Circle(x, y) {
-  var graphics = new PIXI.Graphics();
+  var circle = new PIXI.Graphics();
 
   // draw a circle
-  graphics.lineStyle(2, 0x0033cc);
-  graphics.beginFill(0xffff66, 0.7);
-  graphics.drawCircle(470, 200, radius=10);
-  graphics.endFill();
+  circle.lineStyle(2, 0x0033cc);
+  circle.beginFill(0xffff66, 0.7);
+  circle.drawCircle(470, 200, radius=10);
+  circle.endFill();
 
-  stage.addChild(graphics);
+  circle.position.x = x;
+  circle.position.y = y;
 
-  graphics.position.x = x;
-  graphics.position.y = y;
-  return graphics;
+  stage.addChild(circle);
+
+  circle.interactive = true;
+  circle.on('mousedown', onDragStart)
+    .on('touchstart', onDragStart)
+    // events for drag end
+    .on('mouseup', onDragEnd)
+    .on('mouseupoutside', onDragEnd)
+    .on('touchend', onDragEnd)
+    .on('touchendoutside', onDragEnd)
+    // events for drag move
+    .on('mousemove', onDragMove)
+    .on('touchmove', onDragMove);
+
+  return circle;
 }
 
 var circle = Circle(10, 60);
 
-// Just click on the stage to draw random lines
-stage.on('click', onClick);
-stage.on('tap', onClick);
-
-function onClick()
+function onDragStart(event)
 {
-  graphics.lineStyle(Math.random() * 30, Math.random() * 0xFFFFFF, 1);
-  graphics.moveTo(Math.random() * 620,Math.random() * 380);
-  graphics.bezierCurveTo(Math.random() * 620,Math.random() * 380,
-                         Math.random() * 620,Math.random() * 380,
-                         Math.random() * 620,Math.random() * 380);
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  console.log(event);
+  this.data = event.data;
+  this.startOffset = this.data.getLocalPosition(this);
+  this.startOffset.x *= this.scale.x
+  this.startOffset.y *= this.scale.y
+  this.alpha = 0.5;
+  this.dragging = true;
 }
+
+function onDragEnd()
+{
+  this.alpha = 1;
+
+  this.dragging = false;
+
+  // set the interaction data to null
+  this.data = null;
+}
+
+function onDragMove()
+{
+  if (this.dragging)
+  {
+    var newPosition = this.data.getLocalPosition(this.parent);
+    this.position.x = (newPosition.x - this.startOffset.x);
+    this.position.y = (newPosition.y - this.startOffset.y);
+  }
+}
+
+
+
 // run the render loop
 animate();
 
 function animate() {
-  circle.position.x = circle.position.x + 1;
+  //circle.position.x = circle.position.x + 1;
   renderer.render(stage);
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 }

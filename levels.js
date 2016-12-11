5e1149aba1ba24ca8f6b15e725a893c405aa523b
Nik Shkrob <n.shkrob+gh@gmail.com>
2016-12-10 21:41:10 -0800

    levels

---
+++ levels.js
@@ -0,0 +1,43 @@
+function level8() {
+  CircleOfCircles(13);
+
+  AddLine(0, 3);
+  AddLine(0, 4);
+  AddLine(0, 5);
+  AddLine(0, 7);
+  AddLine(0, 8);
+  AddLine(0, 10);
+
+  AddLine(1, 3);
+  AddLine(1, 5);
+  AddLine(1, 8);
+
+  AddLine(2, 6);
+  AddLine(2, 7);
+  AddLine(2, 9);
+  AddLine(2, 10);
+  AddLine(2, 11);
+  AddLine(2, 12);
+
+  AddLine(3, 6);
+  AddLine(3, 8);
+  AddLine(3, 10);
+  AddLine(3, 8);
+  AddLine(3, 12);
+
+  AddLine(4, 7);
+  AddLine(4, 10);
+
+  AddLine(5, 7);
+  AddLine(5, 8);
+  AddLine(5, 9);
+  AddLine(5, 11);
+
+  AddLine(6, 10);
+  AddLine(6, 12);
+
+  AddLine(7, 10);
+  AddLine(7, 11);
+
+  AddLine(9, 11);
+}
